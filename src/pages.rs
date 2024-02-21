6ee4b1aaec3889d90432ce552cf3ba7f2bb85a1b
Lurk <bargamon@gmail.com>
2024-02-21 22:04:16 +0100

    apply fmt

--- src/pages.rs
+++ src/pages.rs
@@ -323,8 +323,14 @@ mod test {
         .unwrap();
 
         assert_eq!(pages.keys().len(), 2);
-        assert_eq!(pages.get("/test").unwrap().get_title(), "test 1".to_string());
-        assert_eq!(pages.get("/test2").unwrap().get_title(), "test 2".to_string());
+        assert_eq!(
+            pages.get("/test").unwrap().get_title(),
+            "test 1".to_string()
+        );
+        assert_eq!(
+            pages.get("/test2").unwrap().get_title(),
+            "test 2".to_string()
+        );
         assert_eq!(pages.get_tags().len(), 4);
     }
 }
